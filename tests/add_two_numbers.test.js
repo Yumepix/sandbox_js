0bdac14cb9724b89d3f3fd538cbf31aa895057fe
xanderindev <xanderindev@pm.me>
2022-02-20 18:15:38 +0100

    add two number ok

--- tests/add_two_numbers.test.js
+++ tests/add_two_numbers.test.js
@@ -6,18 +6,36 @@ describe('addTwoNumbers', function() {
     let numbers;
     beforeEach(function() {
         numbers = new Numbers();
+
     });
 
     it('Example 1', function () {
-        assert.deepEqual(numbers.addTwoNumbers([2,4,3],[5,6,4]), [7,0,8]);
+        l1 = numbers.tabToList([2,4,3])
+        l2 = numbers.tabToList([5,6,4])
+        l3 = numbers.tabToList([7,0,8].reverse())
+        assert.deepEqual(numbers.addTwoNumbers(l1,l2), l3);
     });
 
     it('Example 2', function () {
-        assert.deepEqual(numbers.addTwoNumbers([0],[0]), [0]);
+        l1 = numbers.tabToList([0])
+        l2 = numbers.tabToList([0])
+        l3 = numbers.tabToList([0].reverse())
+        assert.deepEqual(numbers.addTwoNumbers(l1,l2), l3);
     });
 
     it('Example 3', function () {
-        assert.deepEqual(numbers.addTwoNumbers([9,9,9,9,9,9,9],[9,9,9,9]), [8,9,9,9,0,0,0,1]);
+        l1 = numbers.tabToList([9,9,9,9,9,9,9])
+        l2 = numbers.tabToList([9,9,9,9])
+        l3 = numbers.tabToList([8,9,9,9,0,0,0,1].reverse())
+        assert.deepEqual(numbers.addTwoNumbers(l1,l2), l3);
+    });
+
+
+    it('Example 3', function () {
+        l1 = numbers.tabToList([1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1])
+        l2 = numbers.tabToList([4,6,5])
+        l3 = numbers.tabToList([6,6,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1].reverse())
+        assert.deepEqual(numbers.addTwoNumbers(l1,l2), l3);
     });
 
 });
