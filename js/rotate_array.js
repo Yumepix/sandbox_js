49e9629221cb28fb8c16b47422f51fd2ecc9fa3c
xanderindev <xanderindev@pm.me>
2022-02-20 20:44:50 +0100

    preparation rotate

---
+++ js/rotate_array.js
@@ -0,0 +1,11 @@
+// Rotate array leetcode
+// Given an array, rotate the array to the right by k steps, where k is non-negative.
+class MyClass {
+
+    rotate(nums, k) {
+        return [5,6,7,1,2,3,4]
+    }
+
+}
+
+module.exports = MyClass
